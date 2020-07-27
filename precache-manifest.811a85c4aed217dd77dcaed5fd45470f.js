374f68288e533fc77d1d212f5038e483e6215c64
Miky <mikedorival@gmail.com>
2020-07-27 14:19:00 +0200

    Updates

---
+++ precache-manifest.811a85c4aed217dd77dcaed5fd45470f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc952f45a6c09f35ebc09d98676ed5fd",
+    "url": "/tu-connais/index.html"
+  },
+  {
+    "revision": "ac659941a3abf87cd643",
+    "url": "/tu-connais/static/css/main.2efd1250.chunk.css"
+  },
+  {
+    "revision": "bee680f0af8eb7b00788",
+    "url": "/tu-connais/static/js/2.08885065.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tu-connais/static/js/2.08885065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac659941a3abf87cd643",
+    "url": "/tu-connais/static/js/main.6fa79fee.chunk.js"
+  },
+  {
+    "revision": "e7ea6166d0443a21ceac",
+    "url": "/tu-connais/static/js/runtime-main.98e9716f.js"
+  }
+]);
